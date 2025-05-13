ac522877f8d6947240cdb0fc40426dc9f2913d3e
Prachi Sachdeva <143718652+prachi-308@users.noreply.github.com>
2025-05-13 12:11:11 +0530

    Update script.js

--- src/pages/home/script.js
+++ src/pages/home/script.js
@@ -235,7 +235,7 @@ function resetProgress() {
 
 function startAutoSlide() {
     stopAutoSlide();
-    autoSlideInterval = setInterval(nextSlide, 1000); // Changed from 1500ms to 2000ms
+    autoSlideInterval = setInterval(nextSlide, 1500); // Changed from 1500ms to 2000ms
 }
 
 function stopAutoSlide() {
